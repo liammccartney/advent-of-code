f5355b2d33c24defbd7b48c59d15784433f1738c
Liam McCartney <liam.mccartney@gmail.com>
2020-12-03 18:25:49 -0500

    Complete Day 2, Part 2

--- day_2/solution.py
+++ day_2/solution.py
@@ -1,32 +1,67 @@
+"""
+Day 2
+=====
+Passwords & Policies
+"""
 import re
 
 
-def part_1(policy_and_passwords_list):
+def main(policy_and_passwords_list, validity_function):
+    """
+    Does the thing
+    """
     valid = 0
     for policy_and_password in policy_and_passwords_list:
-        pattern = re.compile(
-            r"^(?P<min>\d+)-(?P<max>\d+) (?P<letter>[a-z]{1}): (?P<pw>[a-z]+)$"
+        (letter, digit1, digit2, password) = extract_policy_and_password(
+            policy_and_password
         )
-        match = re.match(pattern, policy_and_password)
-
-        (letter, min_count, max_count, password) = (
-            match.group("letter"),
-            match.group("min"),
-            match.group("max"),
-            match.group("pw"),
-        )
-
-        if is_password_valid(min_count, max_count, letter, password):
+        if validity_function(digit1, digit2, letter, password):
             valid += 1
 
     return valid
 
 
-def is_password_valid(min_count, max_count, letter, password):
+def password_is_valid_part_1(min_count, max_count, letter, password):
+    """
+    Part 1
+    Validity Determined by Letter Count
+    """
     return int(min_count) <= len([x for x in password if x == letter]) <= int(max_count)
 
 
+def password_is_valid_part_2(pos_1, pos_2, letter, password):
+    """
+    Part 2
+    Validity Determined by Letter Position
+    """
+    idx_1 = int(pos_1) - 1
+    idx_2 = int(pos_2) - 1
+    letter_1 = password[idx_1]
+    letter_2 = password[idx_2]
+
+    return (letter_1 is letter or letter_2 is letter) and (letter_1 is not letter_2)
+
+
+def extract_policy_and_password(policy_and_password):
+    """
+    Extracting letter, digits, and password from input data
+    """
+    pattern = re.compile(
+        r"^(?P<digit1>\d+)-(?P<digit2>\d+) (?P<letter>[a-z]{1}): (?P<pw>[a-z]+)$"
+    )
+
+    match = re.match(pattern, policy_and_password)
+
+    return (
+        match.group("letter"),
+        match.group("digit1"),
+        match.group("digit2"),
+        match.group("pw"),
+    )
+
+
 if __name__ == "__main__":
-    with open("input.txt", "r") as passwords:
-        policy_and_passwords_list = passwords.readlines()
-        print(part_1(policy_and_passwords_list))
+    with open("input.txt", "r") as input_data:
+        input_data = input_data.readlines()
+        print("Part 1:", main(input_data, password_is_valid_part_1))
+        print("Part 2:", main(input_data, password_is_valid_part_2))
