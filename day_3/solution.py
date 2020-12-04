283c925b6c9621726a924f3c66b8fb5268cb0740
Liam McCartney <liam.mccartney@gmail.com>
2020-12-03 22:04:24 -0500

    Complete Day 3, Part 2

--- day_3/solution.py
+++ day_3/solution.py
@@ -3,25 +3,35 @@ Day 3
 Tobogganing
 """
 
+from collections import namedtuple
+from functools import reduce
 
-def main(field):
+
+def main(field, slope):
     """
-    Fin
+    How many trees are you gonna smack into?
     """
     trees = 0
     x = 0
-    for row in field:
-        if row[x] == "#":
+    y = 0
+    width = len(field[0])
+    while y < len(field):
+        if field[y][x] == "#":
             trees += 1
 
-        if x + 3 > len(row) - 1:
-            x = (x + 3) - len(row)
-        else:
-            x += 3
+        x += slope.x
+        if x > width - 1:
+            x -= width
+
+        y += slope.y
 
     return trees
 
 
 if __name__ == "__main__":
+    Slope = namedtuple("Slope", ("x", "y"))
+    slopes = [Slope(1, 1), Slope(3, 1), Slope(5, 1), Slope(7, 1), Slope(1, 2)]
+
     with open("input.txt", "r") as field_data:
-        print(main(field_data.read().splitlines()))
+        field = field_data.read().splitlines()
+        print(reduce(lambda x, y: x * y, [main(field, slope) for slope in slopes]))
