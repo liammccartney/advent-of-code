834c9389e49c0e41859b7b14e9f2932d6d55081c
Liam McCartney <liam.mccartney@gmail.com>
2020-12-06 12:50:56 -0500

    Complete Day 6, Part 2

--- day_6/solution.py
+++ day_6/solution.py
@@ -1,7 +1,7 @@
 import re
 
 
-def main(answers):
+def part_1(answers):
     return sum(
         [
             len(set(list(re.findall(r"[a-z]", answer_group))))
@@ -10,7 +10,27 @@ def main(answers):
     )
 
 
+def part_2(answers):
+    total = 0
+    for answer_group in answers.split("\n\n"):
+        person_answers = answer_group.split("\n")
+        person_1 = set(person_answers[0])
+        total += len(count_all_answered(person_answers[1:], person_1))
+
+    return total
+
+
+def count_all_answered(answer_group, answer_set):
+    if len(answer_group) == 0:
+        return answer_set
+
+    return count_all_answered(
+        answer_group[1:], answer_set.intersection(set(answer_group[0]))
+    )
+
+
 if __name__ == "__main__":
     with open("input.txt", "r") as data:
         data = data.read()
-        print(main(data))
+        print("Part 1:", part_1(data))
+        print("Part 2:", part_2(data))
