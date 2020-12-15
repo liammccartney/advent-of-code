2399094d8cdd0d0f6a640ea6eef5895572fa4096
Liam McCartney <liam.mccartney@gmail.com>
2020-12-15 17:55:33 -0500

    Day 13, Part 2: Solution working for example Set

--- day_13/solution.py
+++ day_13/solution.py
@@ -3,6 +3,7 @@ Day 13
 ======
 Shuttle Search
 """
+from time import time
 
 
 def part_1(earliest_departure, schedule):
@@ -23,8 +24,33 @@ def part_1(earliest_departure, schedule):
     return bus_id * (departure - earliest_departure)
 
 
+def part_2(schedule):
+    t = 0
+    first_bus, bus_tail = int(schedule[0]), schedule[1:]
+    span_found = False
+
+    while span_found is False:
+        if all(
+            future_bus == "x" or (t + i + 1) % int(future_bus) == 0
+            for i, future_bus in enumerate(bus_tail)
+        ):
+            span_found = True
+        else:
+            t += first_bus
+
+    return t
+
+
 if __name__ == "__main__":
+    # with open("example.txt", "r") as dataset:
+    #     dataset = dataset.read().splitlines()
+    #     print("Part 2:", part_2(dataset[1].split(",")))
+
     with open("input.txt", "r") as dataset:
         dataset = dataset.read().splitlines()
-        filtered_schedule = [int(t) for t in dataset[1].split(",") if t != "x"]
-        print("Part 1:", part_1(int(dataset[0]), filtered_schedule))
+        # filtered_schedule = [int(t) for t in dataset[1].split(",") if t != "x"]
+        # print("Part 1:", part_1(int(dataset[0]), filtered_schedule))
+
+        start = time()
+        print("Part 2:", part_2(dataset[1].split(",")))
+        print("Elapsed:", time() - start)
