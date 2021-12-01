dfed959f281a8847cb56322fb65f728a2483f0e6
Liam McCartney <liam.mccartney@gmail.com>
2021-12-01 12:09:16 -0500

    [2021] [Python] Implement Day 01

---
+++ 2021/day_01/solution.py
@@ -0,0 +1,31 @@
+def part_1(depths):
+    increases = 0
+    for i, d in enumerate(depths):
+        if i == len(depths) - 1:
+            return increases
+
+        next_d = depths[i + 1]
+
+        if next_d > d:
+            increases += 1
+
+
+def part_2(depths):
+    window_sums = []
+    for i, d in enumerate(depths):
+        if i >= len(depths) - 2:
+            continue
+
+        next_d = depths[i + 1]
+        next_next_d = depths[i + 2]
+
+        window_sums.append(d + next_d + next_next_d)
+
+    return part_1(window_sums)
+
+
+if __name__ == "__main__":
+    with open("input.txt", "r") as depths:
+        depths = [int(d) for d in depths.readlines()]
+        print("Part 1:", part_1(depths))
+        print("Part 2:", part_2(depths))
