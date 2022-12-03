9d3e8746e134bf0bb1f37dd95c917ea7cfcf905a
Liam McCartney <liam.mccartney@gmail.com>
2022-12-03 11:46:53 -0500

    Day03: Init

---
+++ 2022/elixir/lib/advent2022/day_03.ex
@@ -0,0 +1,18 @@
+defmodule Advent2022.Day03 do
+  def part_1(data) do
+    :todo
+  end
+
+  def part_2(data) do
+    :todo
+  end
+
+  def parse(data) do
+    :todo
+  end
+
+  def main(data) do
+    data = parse(data)
+    {part_1(data), part_2(data)}
+  end
+end
